1e5e9e3d2852f7f04b1f889bba024b02de733cc6
radinahm <54868742+radinahm@users.noreply.github.com>
2022-10-10 19:34:34 -0400

    webpage now renders two different html files

--- webpage.py
+++ webpage.py
@@ -1,11 +1,16 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template
 
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return "Hello, World"
+    return render_template('index.html')
+
+@app.route('/game')
+def index2():
+    return render_template('index2.html')
+
 
 if __name__ == "__main__":
     app.run(debug=True)
