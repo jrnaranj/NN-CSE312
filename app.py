aeb1cc1ba0eca764a201fecdeb031ca70eba4ad2
Adrian Carter <scarter5@buffalo.edu>
2022-10-10 20:06:32 -0400

    gluing the two tasks together

--- app.py
+++ app.py
@@ -1,4 +1,16 @@
-print("Hello Gamers :)")
+from flask import Flask, render_template
 
-while(True):
-    continue
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/game')
+def index2():
+    return render_template('index2.html')
+
+
+if __name__ == "__main__":
+    app.run(debug=True, host="0.0.0.0", port=7878)
