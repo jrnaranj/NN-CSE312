767b44f984064dfd2aacaef97c7d0cdb7be1415b
jrnaranj <60708726+jrnaranj@users.noreply.github.com>
2022-10-11 21:02:06 -0400

    Added database functionality

--- app.py
+++ app.py
@@ -1,4 +1,23 @@
 from flask import Flask, render_template
+from pymongo import MongoClient
+import os
+from dotenv import load_dotenv
+
+load_dotenv('.env')
+password = os.environ.get("PASSWORD")
+print(password)
+
+
+
+connection_string = "mongodb+srv://CSE312_NN:{}@cse312.pezegun.mongodb.net/test".format(password)
+client = MongoClient(connection_string)
+
+db = client["CSE312_Game"] #The name of the database is CSE312_Game
+#db.create_collection("Game Data") #Method to create a new collection in the CSE312_Game database.
+
+
+
+print(connection_string)
 
 
 app = Flask(__name__)
